6ac21a57c76923fc5a56dde9a6ffd77b2bf7eace
Bo Yin <boyin@dyn-160-39-198-192.dyn.columbia.edu>
2015-02-03 20:56:44 -0500

    WIP

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,38 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-
-router.get('/', function(req, res, next) {
-	sql.query('SELECT * FROM store WHERE store_id=' + req.query.id +
-	' AND address_id=' + req.query.addr)
-	.then(function(query_res) {
-	res.send(query_res);
+/* GET rental listing. */
+var pagination = function(req,res){
+	sql.query({
+	    sql: 'SELECT * FROM rental',
+	    nestTables: true,
+	    paginate: {
+	        page: 1+ req.query.offset/req.query.limit,
+	        resultsPerPage: req.query.limit 
+	    }
+	}).then(function(query_res) {
+	// res.send(query_res);
+	var links = [
+			{rel:"next", href:req.params.name}
+	]
+	var pagination_res = {data:query_res, links:links};
+	res.send(pagination_res);
 	});
-});
+};
 
 
-router.get('/store/manager/:id', function(req, res) {
-	sql.query('SELECT * FROM store WHERE manager_staff_id=' + req.params.id)
-	.then(function(query_res) {
-	res.send(query_res);
-	});
-});
 
-// router.get('/actor&offset=12&limit=0', pagination});
-
-// var pagination = function(req, res) {
-// 	sql.query({
-// 	    sql: 'SELECT * FROM ' + req.query.name + ' FROM ' + 
-// 	    req.query.offset + ' TO ' + req.query.limit + req.query.offset,
-	    
-// 	    nestTables: true,
-// 	    paginate: {
-// 	        page: 1,
-// 	        resultsPerPage: req.query.limit
-// 	    }
-// 	}).then(function(query_res) {
-// 		res.send(query_res);
-// 	});
+router.get('/', pagination);
+//e.g http://localhost:3000/rentals?offset=0&limit=10
 
 module.exports = router;
